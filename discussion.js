54c202dbc9cedf89fc4a45d3d9d931f8c7027524
kellyegan <kelly.egan@gmail.com>
2017-11-14 08:56:12 -0500

    Add makeChoice() to pick from sample

--- discussion.js
+++ discussion.js
@@ -3,10 +3,21 @@ const util = require("./utilities")
 /**
  *   Create a discussion from a list of concepts, agents, and concept map
  */
-exports.createDiscussion = function (imageConcepts, agents, conceptMap) {
+exports.createDiscussion = function (conceptPool, agents = null, conceptMap = null) {
   let discussion = {}
 
-  //TODO: implement discussion
+  if( agents == null) {
+    agents = {}
+  }
+  discussion.agents = agents
+
+  if(conceptMap == null) {
+    conceptMap = {}
+  }
+  discussion.conceptMap = conceptMap
+  discussion.stats = {}
+
+  discussion.statements = []
 
   return discussion
 }
@@ -14,9 +25,30 @@ exports.createDiscussion = function (imageConcepts, agents, conceptMap) {
 /**
  *  Make a new hypothesis about an image. Category list give categories to consider
  */
-exports.makeHypothesis = function (conceptPool, categories = null) {
-  if( categories == null ) {
-    categories = Array.from(conceptPool.keys())
+exports.makeHypothesis = function (conceptPool, type = null) {
+  if( type == null ) {
+    type = util.chooseRandom(["Image type", "People", "Building", "Vehicle", "Object"])
+  }
+
+  let hypothesis = null
+
+  switch (type) {
+    case "Image type":
+  
+      break
+    case "People":
+      //["People", "Quantity", "Gender", "Age", "Profession"]
+
+      break
+    case "Building":
+
+      break
+    case "Vehicle":
+
+      break
+    case "Object":
+
+      break
   }
 
   category = util.chooseRandom( categories )
@@ -24,8 +56,43 @@ exports.makeHypothesis = function (conceptPool, categories = null) {
   const choices = conceptPool.get(category)
   if( choices ) {
     const choice = util.chooseRandom(choices)
-    return {category: category, hypothesis: choice}
+    return {type: "hypothesis", hypothesis: [{category: category, choice: choice}]}
   }
 
   return null
+}
+
+/**
+ *  Make a choice by sampling
+ */
+exports.makeChoice = function ( conceptPool, category ) {
+  let goal = 3
+  let tests = 5
+  let noWinner = true
+  let results = new Map()
+  let choice = null
+
+  while( noWinner ) {
+    for( let i = 0; i < tests; i++ ){
+      let pick = util.chooseRandom(conceptPool.get(category))
+      if( results.has(pick) ) {
+        let count = results.get( pick ) + 1
+        results.set( pick, count )
+        if( count >= goal ) {
+          noWinner = false
+          choice = pick
+        }
+      } else {
+        results.set( pick, 1 )
+      }
+    }
+    if( noWinner ) {
+      goal += 1
+      tests += 2
+    }
+    if(tests > 7) {
+      return null
+    }
+  }
+  return { choice: choice, certainty: results.get(choice) / tests }
 }
